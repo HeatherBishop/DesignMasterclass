323197de6336723c35997feb15d71eabe2c4de89
HeatherBishop <s177801@ucs.ac.uk>
2017-12-30 15:24:25 +0000

    scene selection

---
+++ UnityProjects/TimeLine/Assets/Scripts/SceneSelection.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSelection : MonoBehaviour {
+
+	//Possibly the laziest code you have ever seen.
+	//Dont judge me.
+	//PS thanks for commenting your code :P 
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//load main menu
+	public void LoadMainMenu()
+	{
+		SceneManager.LoadScene ("menu", LoadSceneMode.Single);
+	}
+
+	//load main scene
+
+	public void LoadMainScene()
+	{
+		SceneManager.LoadScene ("scene", LoadSceneMode.Single);
+	} 
+
+	//load win screen
+	public void LoadWinScreen()
+	{
+		SceneManager.LoadScene ("end", LoadSceneMode.Single);
+	}
+
+}
